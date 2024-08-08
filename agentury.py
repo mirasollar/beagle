95e4b55727511ba9a90eabdaad78bb6ade90fcb1
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2024-08-08 16:22:27 +0200

    Update agentury.py

--- agentury.py
+++ agentury.py
@@ -8,12 +8,9 @@ import os
 
 
 uploaded_file = st.file_uploader("Choose a file")
-st.write(f"Uploaded file: {uploaded_file}")
 
-if uploaded_file is not None and uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-    df_raw = pd.read_excel('rev.xlsx', nrows=20)
-    header_loc = df_raw[df_raw == 'Agentura'].dropna(axis=1, how='all').dropna(how='all')
-    header_row = header_loc.index.item()
-    df_agentury = pd.read_excel('rev.xlsx', header=header_row+1)
-    st.write(df_agentury)
-    st.write(os.listdir())
+
+if uploaded_file is not None:
+    st.write(f"Soubor je: {uploaded_file}")
+
+    st.write(f"Directory je: {os.listdir()}")
